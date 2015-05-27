8e8d0d08584b64a65b5766c7a4527750b9992f2e
hamsterofdark <mlandryviolin@gmail.com>
2015-05-27 09:36:57 -0400

    Added output to jsp

--- src/hamsterofdark/action/TutorialAction.java
+++ src/hamsterofdark/action/TutorialAction.java
@@ -2,10 +2,18 @@ package hamsterofdark.action;
 
 public class TutorialAction
 {
+	private String bestTutorialSite;
 	public String execute()
 	{
+		setBestTutorialSite("Udacity");
 		System.out.println("execute fired");
 		return "success";
+	}
+	public String getBestTutorialSite() {
+		return bestTutorialSite;
+	}
+	public void setBestTutorialSite(String bestTutorialSite) {
+		this.bestTutorialSite = bestTutorialSite;
 	}
 
 }
